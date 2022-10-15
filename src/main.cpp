640b72e9fc6bc859301e179f3a4ec7793c0cc107
kentril0 <kentril.despair@gmail.com>
2022-10-15 23:11:42 +0200

    Running waterSurface app

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 
 #include "pch.h"
-//#include "WaterSurface.h"
-#include "core/Application.h"
+#include "WaterSurface.h"
 
 
 int main(int argc, char** argv)
@@ -9,18 +8,10 @@ int main(int argc, char** argv)
     vkp::Log::Init();
     VKP_LOG_TRACE("Initialized Logging");
 
-    vkp::Application app("Water Surface Rendering", { argc, argv });
-    app.Run();
-
-    /*
     {
-        WaterSurface app("Water Surface Rendering", { argc, argv });
-
-        app.Init();
-            app.Run();
-        app.Cleanup();
+        WaterSurface app({ argc, argv });
+        app.Run();
     }
-    */
 
     return EXIT_SUCCESS;
 }
